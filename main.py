57abf7b519b5efa357e96e4ce636797ed91af140
Loodoor <folaefolc@outlook.fr>
2016-07-01 21:57:14 +0200

    correctif main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ win_manager = system.wm.DesktopManager(win)
 system.process_manager.ProcessManager()
 
 system.process_manager.ProcessManager.add_windows(*[_ for _ in system.app_list + apps.app_list])
-system.process_manager.ProcessManager.init_windows_with(screen)
+system.process_manager.ProcessManager.init_windows_with(win)
 win_manager.run()
 
 pygame.quit()
